cc9c1c5fa66639f8ff11571decdcc0342e6b76d4
Yanina666 <RUZETKA-BESPAROLKA@rambler.ru>
2023-04-28 16:18:08 +0300

    new commit

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args)
+    {
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("    |    |    ");
+    }
+}
